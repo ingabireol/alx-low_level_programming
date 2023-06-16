a418440d7f297834e2f62624f2ad93fe6e8ed1b5
ingabireol <ingabire069@gmail.com>
2023-06-16 02:31:28 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit of base 10
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
