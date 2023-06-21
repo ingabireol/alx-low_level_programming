bc147b11583a512af08538aed3c8986aacb7fe68
ingabireol <ingabire069@gmail.com>
2023-06-20 23:27:00 -0700

    Prints lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints an alphabet
+ *
+ * Return: void.
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+
