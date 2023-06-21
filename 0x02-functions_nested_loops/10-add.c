6978d33d9fd1f064b224cb5a040b7a31ae2af820
ingabireol <ingabire069@gmail.com>
2023-06-21 08:58:11 -0700

    adds teo numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  add - adds two integers
+ *
+ * @a: first integer
+ * @b: second intger
+ *
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
