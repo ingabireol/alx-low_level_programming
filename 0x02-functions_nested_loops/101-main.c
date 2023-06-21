20092337a5980a2dcb26f52d50d2913ea2b4745a
ingabireol <ingabire069@gmail.com>
2023-06-21 14:31:49 -0700

    sum of 0-1024 numbers

---
+++ 0x02-functions_nested_loops/101-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the sum of multiples of 3 and 5 to 1024
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_natural();
+	return (0);
+}
+
