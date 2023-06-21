1de25fd409755045abbe7274f74cca5985aff86c
ingabireol <ingabire069@gmail.com>
2023-06-21 12:20:50 -0700

    Prints time table to n number

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,18 +1,78 @@
 #include "main.h"
 
 /**
- * print_times_table - prints n time table
- * @n: number of times table
+ * print_less10 - Handles numbers less than 10 in the time table.
+ * @i: first number.
+ * @j: second number.
  *
  * Return: void
  */
+void print_less10(int i, int j)
+{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar((j * i) + '0');
 
+}
+/**
+ * print_great100 - Handles number greater 100 in the time table.
+ * @i: num1.
+ * @j: num2.
+ *
+ * Return: void
+ */
+void print_great100(int i, int j)
+{
+	if (j == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+	}
+	_putchar(' ');
+	_putchar(((i * j) / 100) + '0');
+	_putchar((((i * j) / 10) % 10) + '0');
+	_putchar(((i * j) % 10) + '0');
+}
+/**
+ * print_less100 - Handles number with product > 999 in the table.
+ * @i: num1.
+ * @j: num2.
+ *
+ * Return: void
+ */
+void print_less100(int i, int j)
+{
+	if (j == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+	}
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(((i * j) / 10) + '0');
+	_putchar(((i * j) % 10) + '0');
+
+}
+/**
+ * print_times_table - prints n time table
+ * @n: number of times table
+ *
+ *
+ * Return: void
+ */
 void print_times_table(int n)
 {
 	int i, j;
 
 	if (n  > 15 || n < 0)
 	{
+		return;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
 		return;
 	}
 	for (i = 0; i <= n; i++)
@@ -22,48 +82,22 @@ void print_times_table(int n)
 			if (j == 0)
 			{
 				_putchar('0');
-				_putchar(',');
 			}
 			else if ((i * j) < 10)
 			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((j * i) + '0');
-				if (j == n)
-					continue;
-				_putchar(',');
+				print_less10(i, j);
 			}
 			else if ((i * j) >= 10 && (i * j) < 100)
 			{
-				if (j == 0)
-				{
-					_putchar('0');
-					_putchar(',');
-				}
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(((i * j) / 10) + '0');
-				_putchar(((i * j) % 10) + '0');
-				if (j == n)
-					continue;
-				_putchar(',');
+				print_less100(i, j);
 			}
 			else if ((i * j) >= 100)
 			{
-				if (j == 0)
-				{
-					_putchar('0');
-					_putchar(',');
-				}
-				_putchar(' ');
-				_putchar(((i * j) / 100) + '0');
-				_putchar((((i * j) / 10) % 10) + '0');
-				_putchar(((i * j) % 10) + '0');
-				if (j == n)
-					continue;
-				_putchar(',');
+				print_great100(i, j);
 			}
+			if (j == n)
+				continue;
+			 _putchar(',');
 		}
 		_putchar('\n');
 	}
