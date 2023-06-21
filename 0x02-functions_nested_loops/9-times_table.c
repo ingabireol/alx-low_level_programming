013f9d92e362da93d11d46071b3fa6ba53ea6246
ingabireol <ingabire069@gmail.com>
2023-06-21 08:51:02 -0700

    Prints 9 tim tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,25 +14,31 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
+			if (j == 0)
+			{
+				_putchar('0');
+				_putchar(',');
+				continue;
+			}
 			if ((i * j) < 10)
 			{
+				_putchar(' ');
+				_putchar(' ');
 				_putchar((i * j) + '0');
 				if (j == 9)
 				{
 					continue;
 				}
 				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
 			}
 			else
 			{
+				_putchar(' ');
 				_putchar(((i * j) / 10) + '0');
 				_putchar(((i * j) % 10) + '0');
 				if (j == 9)
 					continue;
 				_putchar(',');
-				_putchar(' ');
 			}
 		}
 		_putchar('\n');
