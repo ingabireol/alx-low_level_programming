57f78e99de55cb7cc2a88706b237f9f75429eaa1
ingabireol <ingabire069@gmail.com>
2023-06-20 23:48:34 -0700

    print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets 10 times
+ *
+ * Return: void
+ *
+ */
+
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	for (i = 1; i < 11; i++)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}
