fb0ce5ddb893d3e4bf80298f31287c4f71ad0e44
ingabireol <ingabire069@gmail.com>
2023-06-21 02:00:22 -0700

    prints the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,6 +2,7 @@
 
 /**
  * print_last_digit - prints the last digit of anumebr
+ *@n: the number to fid its last disgit
  *
  * Return: the last digit of the number
  */
@@ -10,8 +11,11 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
+		n = n % 10;
 		n = -n;
+		_putchar(n + '0');
+		return (n);
 	}
-	_putchar((n%10) + '0');
-	return (n%10);
+	_putchar((n % 10) + '0');
+	return (n % 10);
 }
