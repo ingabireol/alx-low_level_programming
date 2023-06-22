33687f731d42920b60a8c9ed35cf9892e9acce60
ingabireol <ingabire069@gmail.com>
2023-06-21 22:21:44 -0700

    Sum of Fibonacci number;

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum od fibonacci numbers less than 4000000
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int temp, i, j, k = 0, sum = 0;
+
+	for (i = 1, j = 0; k < 50; k++)
+	{
+		temp = i + j;
+		j = i;
+		i = temp;
+		if ((temp % 2) == 0 && (temp < 4000000))
+		{
+			sum += temp;
+		}
+	}
+	printf("%ld\n", sum);
+	return 0;
+}
