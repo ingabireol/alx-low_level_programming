68ca01469680a5ac214f1643d8fd1565e60a2400
ingabireol <ingabire069@gmail.com>
2023-06-21 22:43:12 -0700

    98 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints first fibonacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	double  temp, i, j, c = 96;
+
+	printf("1, 2\n");
+	for (i = 2, j = 1; c > 0; c--)
+	{
+		if (c == 1)
+		{
+			temp = i + j;
+			printf("%.0f", temp);
+			continue;
+		}
+		temp = i + j;
+		printf("%.0f, ", temp);
+		j = i;
+		i = temp;
+	}
+	printf("\n");
+	return (0);
+}
