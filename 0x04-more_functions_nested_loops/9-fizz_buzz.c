23b2304215efd3b9146b3f85760524de33aa7bf6
ingabireol <ingabire069@gmail.com>
2023-06-22 08:19:09 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
@@ -21,10 +25,7 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
+
 		else
 			printf("%d ", i);
 	}
