59ecc25195ae9e3486a6de8344827beb8c76b17c
ingabireol <ingabire069@gmail.com>
2023-06-27 05:13:24 -0700

    Reversing a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * rev_string - print the string in reverse
+ * @s: string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = _strlen(s) - 1;
+	char temp;
+
+	while (s[i] != '\0')
+	{
+		if (i == j)
+			break;
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		j--;
+		i++;
+	}
+}
+
+/**
+ * _strlen - calculates the length of the string
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
