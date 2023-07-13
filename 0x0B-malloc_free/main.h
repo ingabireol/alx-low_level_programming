6d332d0885051f8a6bcf06db1f04c7f3ce123134
ingabireol <ingabire069@gmail.com>
2023-07-12 17:56:51 -0700

    Initial commit

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,4 +4,5 @@ char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 #endif /* MAIN_H */
