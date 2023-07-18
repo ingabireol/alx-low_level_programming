e1b6a84cacb1588aa2c2b945896d845f05d3e009
ingabireol <ingabire069@gmail.com>
2023-07-18 05:28:26 -0700

    Initual commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+typedef struct dog dog_t;
+
+/**
+ * struct dog - structure to hold dog info
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the dog owner
+ *
+ * Description: Holding the structre for dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
