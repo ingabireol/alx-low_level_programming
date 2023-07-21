5097fc16ca7c48b85b71f1083c692a5e79c7ab49
ingabireol <ingabire069@gmail.com>
2023-07-20 18:21:48 -0700

    initual commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum up the numbers
+ * @n: numbers
+ * Return: Sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i;
+	int sum = 0;
+
+	va_start(num, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(num, int);
+	}
+	va_end(num);
+	return (sum);
+}
