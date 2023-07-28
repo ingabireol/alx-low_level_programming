e7190c4057898e693d40462a04de436b419143d1
ingabireol <ingabire069@gmail.com>
2023-07-28 16:53:19 -0700

    add to an end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * leng - finds the length of a string
+ * @str: string
+ * Return: the length of a sting
+ */
+
+int leng(const char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * add_node_end - adds node to an end
+ * @str: string to eb added
+ * @head: the list
+ * Return: list_t of the currnt
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *aux = *head;
+	list_t *tmp = malloc(sizeof(list_t));
+
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+	tmp->len = leng(str);
+	tmp->next = NULL;
+	tmp->str = strdup(str);
+	if (!tmp->str)
+	{
+		free(tmp);
+		return (NULL);
+	}
+	if (aux)
+	{	
+		while (aux->next != NULL)
+			aux = aux->next;
+		aux->next = tmp;
+	}
+	else
+		*head = tmp;
+	return (tmp);
+
+}
