3952a983da9012027caef2ca97c9c9a16f55677c
ingabireol <ingabire069@gmail.com>
2023-07-28 14:28:59 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+About linked lists
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the sytinf
+ * @next: points to the nest node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
+#endif /* LISTS_H */
