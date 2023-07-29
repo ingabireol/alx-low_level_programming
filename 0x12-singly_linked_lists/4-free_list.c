49112bf6db3a92b04dec9e6c6427c9fc2bb19030
ingabireol <ingabire069@gmail.com>
2023-07-28 17:33:35 -0700

    Initual commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - frees a list
+ * @head: the list to be freed
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current_node, *next_node;
+
+	
+	if(head)
+	{
+		current_node = head;
+		next_node = head->next;
+		while (next_node)
+		{
+			free(current_node->str);
+			free(current_node);
+			current_node = next_node;
+			next_node = next_node->next;
+		}
+		free(current_node->str);
+		free(current_node);
+	}
+}
