9b9eb0fcd19489eb8115fda6a6c9ee9bfbafd903
ingabireol <ingabire069@gmail.com>
2023-07-28 17:52:58 -0700

    initual commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) hare(void);
+/**
+ * hare - Prints a string before the
+ * main function is executed.
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, oyu must allow,\nI bore my house upon my back!\n");
+}
